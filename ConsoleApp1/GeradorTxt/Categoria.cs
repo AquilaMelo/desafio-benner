fa4fa7f380404dfb0d00df16a527c1c14a1190e1
AquilaMelo <aquilamelo2002@gmail.com>
2026-01-10 20:37:41 -0300

    Criei a classe Categoria

---
+++ ConsoleApp1/GeradorTxt/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.GeradorTxt
+{
+    public class Categoria
+    {
+        public int NumeroCategoria { get; set; }
+        public string Descricao { get; set; }
+    }
+}
