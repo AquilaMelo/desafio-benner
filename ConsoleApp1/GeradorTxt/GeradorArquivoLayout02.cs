19f3ba08251dee6e3662ff9488fa803d072eb71d
AquilaMelo <aquilamelo2002@gmail.com>
2026-01-10 21:18:56 -0300

    Fiz um override da classe EscreverTipo02 no novo arquivo GeradorArquivoLayout02 que herda GeradorArquivoBase

---
+++ ConsoleApp1/GeradorTxt/GeradorArquivoLayout02.cs
@@ -0,0 +1,29 @@
+﻿using GeradorTxt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.GeradorTxt
+{
+    internal class GeradorArquivoLayout02 : GeradorArquivoBase
+    {
+        protected override void EscreverTipo02(StringBuilder sb, ItemDocumento item)
+        {
+            // 02|NUMEROITEM|DESCRICAOITEM|VALORITEM
+            sb.Append("02").Append("|")
+              .Append(item.NumeroItem).Append("|")
+              .Append(item.Descricao).Append("|")
+              .Append(ToMoney(item.Valor)).AppendLine();
+
+            // 03|NUMEROCATEGORIA|DESCRICAOCATEGORIA
+            foreach (var categoria in item.Categorias)
+            {
+                sb.Append("03").Append("|")
+                  .Append(categoria.NumeroCategoria).Append("|")
+                  .Append(categoria.Descricao).AppendLine();
+            }
+        }
+    }
+}
